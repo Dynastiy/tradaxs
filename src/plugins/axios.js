2f10fb814dae34807e3d2a0718dac4f792bbb50f
Dynastiy <emmanuel00michael@gmail.com>
2022-05-17 12:18:31 +0100

    fixed signup

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import "toastify-js/src/toastify.css"
 import Toastify from 'toastify-js'
-import store from '../store'
-import router from '../router'
+// import store from '../store'
+// import router from '../router'
 Vue.use(Toastify)
 
 import axios from "axios";
-const BASE_URL = 'https://api.buybnb.io/api';
+const BASE_URL = 'https://api.tradaxs.com/api/';
 
 const instance = axios.create({
     baseURL: BASE_URL,
     headers: {
         "Content-type": "application/json",
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Headers": "*",
-        // "Access-Control-Allow-Methods": 'GET, HEAD, PUT, PATCH, POST, DELETE'
-        "Authorization": `bearer ${store.state.token}`
+        // "Authorization": `Bearer 1|WnM7JDwUoOuM3BoCqrDfErQZrw58haoDUvuePhgK`,
+        'web_key': 'd797e46b-14d2-4248-a451-1dd60faa9bd0',
+        'token': '1|WnM7JDwUoOuM3BoCqrDfErQZrw58haoDUvuePhgK',
+        "Access-Control-Allow-Origin": "*",
     }
 });
 
@@ -23,12 +23,14 @@ const instance = axios.create({
 instance.interceptors.request.use(function(config) {
     // Do something before request is sent
     // alert("requesting")
-    console.log(`${store.state.token}`);
-    const token = `${store.state.token}`;
+    // console.log(`1|WnM7JDwUoOuM3BoCqrDfErQZrw58haoDUvuePhgK`);
+    // const token = `1|WnM7JDwUoOuM3BoCqrDfErQZrw58haoDUvuePhgK`;
 
-    if (token) {
-        config.headers['Authorization'] = `bearer ${store.state.token}`;
-    }
+    // if (token) {
+    //     config.headers['Authorization'] = `Bearer ${token}`;
+    //     config.headers['token'] = `${token}`;
+    //     config.headers['web_key'] = `d797e46b-14d2-4248-a451-1dd60faa9bd0`;
+    // }
 
     return config;
 }, function(error) {
@@ -45,19 +47,14 @@ instance.interceptors.response.use(function(response) {
 }, function(error) {
 
     console.log(error.response);
-    // console.log(error.response.data.detail);
-    if (error.response.data.message === 'Unauthenticated.') {
-        router.push('/sign-in')
-            // alert("Hello World")
-    }
     Toastify({
-        text: error.response.data.message,
+        text: error.response,
         className: "info",
         style: {
             background: "red",
         }
     }).showToast();
-    console.log(error.response.data.message)
+    console.log(error.response)
 
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
--- src/views/auth/signUp.vue
+++ src/views/auth/signUp.vue
@@ -3,30 +3,42 @@
         <div class="login">
             <div class="login--container">
                 <div class="login--content">
-                    <div class="logo text-center mb-4">
+                    <div class="logo text-center mb-2">
                         <img src="@/assets/img/logo-dark.svg" width="35" alt="" srcset="">
                         <h3 class="font-weight-bold small text-dark">Tradaxs</h3>
                     </div>
-                    <div class="text-center mb-4">
+                    <div class="text-center mb-3">
                         <h5 class="font-weight-bold text-dark">Create Account</h5>
                         <p class="small p--text">Enter your details to Create New Account</p>
                     </div>
                     <div class="form--content">
-                        <form action="">
-                            <div class="mb-4">
+                        <form @submit.prevent="register">
+                            <div class="mb-2">
                             <label class="mb-2" for="">FULL NAME</label>
-                            <input type="text">
+                            <input type="text" v-model="credentials.name">
                         </div>
-                        <div class="mb-4">
+                        <div class="mb-2">
+                            <label class="mb-2" for="">USERNAME</label>
+                            <input type="text" v-model="credentials.username">
+                        </div>
+                        <div class="mb-2">
                             <label class="mb-2" for="">EMAIL</label>
-                            <input type="email">
+                            <input type="email" v-model="credentials.email">
                         </div>
-                         <div class="mb-4">
+                        <div class="mb-2">
+                            <label class="mb-2" for="">BVN</label>
+                            <input type="number" v-model="credentials.bvn">
+                        </div>
+                        <div class="mb-2">
+                            <label class="mb-2" for="">PHONE NUMBER</label>
+                            <input type="number" v-model="credentials.phone">
+                        </div>
+                         <div class="mb-3">
                             <label for="">PASSWORD</label>
-                            <input type="password">
+                            <input type="password" v-model="credentials.password">
                         </div>
                         <div class="mb-3">
-                            <button class="auth--button" @click="goToLogin">Sign up</button>
+                            <button class="auth--button" >Sign up</button>
                         </div>
                         <div class="text-center">
                             <small class="small font-weight-bold">Already have an account? <router-link to="/login">Login</router-link> </small>
@@ -41,10 +53,34 @@
 
 <script>
 export default {
-    methods:{
-        goToLogin(){
-            this.$router.push('/login')
+    data(){
+        return{
+            credentials:{
+                name: '',
+                username: '',
+                email: '',
+                bvn: '',
+                phone: '',
+                password: ''
+            }
         }
+    },
+    methods:{
+       async register(){
+            let formData = new FormData()
+            formData.append("name", this.credentials.name)
+            formData.append("username", this.credentials.username)
+            formData.append("email", this.credentials.email)
+            formData.append("phone", this.credentials.phone)
+            formData.append("password", this.credentials.password)
+            formData.append("bvn", this.credentials.bvn)
+           try {
+               let res = await this.$axios.post('/auth/register', formData)
+               console.log(res);
+           } catch (error) {
+               console.log(error);
+           }
+       }
     }
 }
 </script>
